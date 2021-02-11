4b58b6fc2a0b363cd5d302bd6008df7c08e51647
akashtech-source <mr.ifranakash@gmail.com>
2021-02-11 22:06:22 +0600

    almost done

---
+++ app.js
@@ -0,0 +1,42 @@
+const searchSong = async() => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+        const res = await fetch(url);
+        const data = await res.json()
+        displaySong(data.data)
+}
+
+const displaySong = song => {
+    const songContainer = document.getElementById("songContainer");
+    songContainer.innerHTML = '';
+    song.forEach(song => {
+        console.log(song);
+        const songDiv = document.createElement('div');
+        songDiv.className = "single-result row align-items-center my-3 p-3"
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                    <audio controls>
+                <source src="${song.preview}" type="audio/ogg">
+            </audio>
+            </div>
+            <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+         </div>
+        `;
+        songContainer.appendChild(songDiv);
+    })
+}
+
+const getLyric = async(artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    const res = await fetch(url);
+    const data = await res.json()
+    displayLyrics(data.lyrics)
+}
+
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById("song-lyrics");
+    lyricsDiv.innerText = lyrics;
+}
